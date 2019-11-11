36900989a688c635bcdc4b0c89d7f3d98702804f
essential-hacker-4489 <akpojotor@yahoo.com>
2019-11-11 19:26:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -74,10 +74,8 @@ def apply_coupons(cart,coupons)
   cart.map do |item1|
     new_hash={}
     coupons.map do |item2|
-      if item1[:item] == item2[:item]
-        if item1[:count]>item2[:num]
-          item1[:count]=item1[:count]-item2[:num]
-        end
+      if item1[:item] == item2[:item] && item1[:count] > item2[:num]
+        item1[:count]=item1[:count]-item2[:num]
         new="#{item1[:item]} W/COUPON"
         new_hash[:item]=new
         new_hash[:price]=item2[:cost]/item2[:num]
